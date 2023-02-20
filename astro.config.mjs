d82a2445fe1c9abedf6c14637ba3393b79b7a434
AleksandrukZakhar <zakharaleksandrukdev@gmail.com>
2023-02-20 11:06:45 +0200

    u

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,6 @@
 import { defineConfig } from "astro/config";
 
-export default defineConfig({});
+export default defineConfig({
+    site: "https://aleksandrukzakhar.github.io/school/",
+    base: "/school/",
+});
